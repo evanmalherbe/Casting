864efdb1ac3bc604daa4b5d5596895186d4c2a8a
evanmalherbe <evanmalherbe@gmail.com>
2022-12-16 08:48:24 +0200

    Add project files.

---
+++ Casting/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Text text = new Text();
+            Shape shape = text;
+
+            text.Width = 200;
+            shape.Width = 100;
+
+            Console.WriteLine("Width: " + text.Width);
+        }
+    }
+}
